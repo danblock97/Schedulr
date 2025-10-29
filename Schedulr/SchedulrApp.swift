439606bd5357d427a46952a700de8f2d7cfce98e
Dan <generalgoldiez@gmail.com>
2025-10-29 12:37:38 +0000

    Initial Commit

---
+++ Schedulr/SchedulrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulrApp.swift
+//  Schedulr
+//
+//  Created by Daniel Block on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SchedulrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SchedulrTests/SchedulrTests.swift
@@ -0,0 +1,16 @@
+//
+//  SchedulrTests.swift
+//  SchedulrTests
+//
+//  Created by Daniel Block on 29/10/2025.
+//
+
+import Testing
+
+struct SchedulrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
